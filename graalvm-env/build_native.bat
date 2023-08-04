f225b18ecb7dc4091265fc0e7b5b51505d6de378
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-08-03 19:06:27 -0700

    Hefty Graal config changes for latest version.
    We need to generate the configuration JSON files using a Gradle task currently, and can't rely on the annotation processor for some unclear reason. Most Graal configuration has moved from core to headless, where it belongs.

--- graalvm-env/build_native.bat
+++ graalvm-env/build_native.bat
@@ -1,12 +1,4 @@
-"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64 && ^
 C:\d\jvm\graal17\bin\native-image.cmd ^
--H:+ReportExceptionStackTraces ^
---report-unsupported-elements-at-runtime ^
+-march=native ^
 --no-fallback ^
--H:ReflectionConfigurationFiles=config/reflect-config.json ^
--H:JNIConfigurationFiles=config/jni-config.json ^
--H:DynamicProxyConfigurationFiles=config/proxy-config.json ^
--H:SerializationConfigurationFiles=config/serialization-config.json ^
--H:ResourceConfigurationFiles=config/resource-config.json ^
--Dorg.lwjgl.librarypath=. ^
--jar spotvox.jar
+-jar ../headless/build/libs/spotvox.jar
---
+++ graalvm-env/setup_build.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
